55eb44b3c6c705a33858fd87ce5bb431c6e98883
Vitaliy Po <76914279+Granddad-Po@users.noreply.github.com>
2022-02-04 13:53:00 +0300

    refact: styles

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ const App: React.FC = () => {
     return (
         <DragDropContext onDragEnd={onDragEnd}>
             <div className='App'>
-                <span className={'heading'}>Taskify</span>
+                <span className={'heading'}>iPlan</span>
                 <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
                 <TodoList todos={todos}
                           setTodos={setTodos}
